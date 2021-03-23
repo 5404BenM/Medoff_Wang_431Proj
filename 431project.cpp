672b77713291d1d581b7831f50ba2dce9463bc45
Benjamin Medoff <medofbro@gmail.com>
2021-03-22 21:22:38 -0400

    added debuging print

--- 431project.cpp
+++ 431project.cpp
@@ -124,6 +124,7 @@ int main(int argc, char** argv) {
 		}
 
 		if (proposedGeoEDP < bestEDP) {
+			std:cout << std::endl << "Prop: " << proposedGeoEDP << " Best: " << bestEDP;
 			bestEDPconfig = nextconf;
 			bestEDP = proposedGeoEDP;
 		}
